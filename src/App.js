ccbe596b28a682e706f7950fbec1aace2c5c066a
Felipe Soto C <pipesoto13@gmail.com>
2020-11-04 14:33:35 -0500

    my solution

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      lastName: '',
+      rows: []
+    };
+  }
+
+    updateName(e){
+      this.setState({
+        name: e.target.value
+      })
+    }
+    
+    updatelastName(e){
+      this.setState({
+        lastName: e.target.value
+      })
+    }
+
+    addGuest(e){
+      e.preventDefault();
+      const guest = {
+        name: this.state.name,
+        lastName: this.state.lastName
+      }
+      const oldRows = this.state.rows;
+      const newRows = oldRows.concat(guest);
+      this.setState({
+        rows: newRows,
+        name: '',
+        lastName: '',
+      });
+    }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addGuest.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.name} onChange={this.updateName.bind(this)}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.updatelastName.bind(this)}/>
               </div>
 
               <div className="action">
@@ -27,10 +65,10 @@ class App extends Component {
                 <tr>
                   <th>Nombre</th>
                   <th>Apellido</th>
-                </tr>
+                </tr>              
               </thead>
               <tbody>
-
+                {this.state.rows.map((row, index) => <tr key={index}><td>{row.name}</td><td>{row.lastName}</td></tr>)}
               </tbody>
             </table>
           </div>
